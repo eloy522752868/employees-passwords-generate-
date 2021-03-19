ffbc9dcc16c234113848a1db13a4c659601b382f
Eloy Gonzalez <eloy522752@gmail.com>
2021-03-19 11:15:49 -0400

    add chaged to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,57 @@
     //Decalared my Varaibles 
     var inputLength = document.querySelector('input[name="length"]');
-    var smallLetter = document.querySelector('input[name="lowercase"]');
-    var bigLetter = document.querySelector('input[name="uppercase"]');
+    var lowerCaseLetter = document.querySelector('input[name="lowercase"]');
+    var upperCaseLetter = document.querySelector('input[name="uppercase"]');
     var number = document.querySelector('input[name="number"]');
     var symbol = document.querySelector('input[name="symbol"]');
-    var generatePassword = document.querySelector('.wrapper button');
+    var generatePass = document.querySelector('.wrapper button');
     
+ passKeys = {
+  lowercase: 'abcdefghijklmnopqrstuvwxyz',
+  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  number: '0123456789',
+  symbol: '*;<>()[]{}#$?!^|'
+};
+
+// Call action
+generatePass.addEventListener("click", () => {
+  var length = +inputLength.value;
+  console.log(inputLength.value );
+  var activeLower = lowerCaseLetter.checked;
+  var activeUpper = upperCaseLetter.checked;
+  var activeNumber = number.checked;
+  var activeSymbol = symbol.checked;
+  generatePassword(activeLower, activeUpper, activeNumber, activeSymbol, length);
+  
+});
+
+function generatePassword(lower, upper, num, sym, length){
+  //Contructing the Passkeyselected
+   selectedPassKeys = "";
+   finalPassword = "";
+  
+    passOptions = {
+    lowercase: lower,
+    uppercase: upper,
+    number: num,
+    symbol: sym
+  };
+
+  console.log(passOptions);
+  for(i=0;i<Object.keys(passOptions).length;i++)
+  {
+    selectedPassKeys += (Object.values(passOptions)[i]) ? passKeys[Object.keys(passOptions)[i]] : "";
+  }
+  console.log(selectedPassKeys);
+  if(selectedPassKeys != "" && length > 0){
+    for(i=0;i<length;i++){
+      finalPassword += selectedPassKeys[Math.floor(Math.random() * selectedPassKeys.length)];
+    }
+    var passwordText = document.querySelector("#password");
+    passwordText.value = finalPassword;
+    //document.querySelector('input[type="text"]').value =  finalPassword;
+  }else{
+    passwordText.value =  "Select any password option and specify the length";
+  }
+  
 }
