7f9e5af7eb4f951cff39756df8401da023c2c1be
Eloy Gonzalez <eloy522752@gmail.com>
2021-03-19 10:50:21 -0400

    Started making code changes wired frame it.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,9 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+    //Decalared my Varaibles 
+    var inputLength = document.querySelector('input[name="length"]');
+    var smallLetter = document.querySelector('input[name="lowercase"]');
+    var bigLetter = document.querySelector('input[name="uppercase"]');
+    var number = document.querySelector('input[name="number"]');
+    var symbol = document.querySelector('input[name="symbol"]');
+    var generatePassword = document.querySelector('.wrapper button');
+    
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
