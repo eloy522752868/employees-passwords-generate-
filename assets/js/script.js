efe880431a0ed94a9744a1e6b7937f0f6532f2c4
Eloy Gonzalez <eloy522752@gmail.com>
2021-03-19 16:12:32 -0400

    added more comments to my javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,26 @@
-    //Decalared my Varaibles 
-    var inputLength = document.querySelector('input[id="length"]');
-    var lowerCaseLetter = document.querySelector('input[id="lowercase"]');
-    var upperCaseLetter = document.querySelector('input[id="uppercase"]');
-    var number = document.querySelector('input[id="number"]');
-    var symbol = document.querySelector('input[id="symbol"]');
-    var generatePass = document.querySelector('.wrapper button');
+    /**
+* Name: Eloy Gonzalez
+* Date: 03/17/2021
+* Description:
+ This week’s homework requires me to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. 
+ This app will run in the browser and will feature dynamically updated HTML and CSS. I was going to use Alerts, prompts and confirm boxes. But after the past week gone bit more on working with API's 
+ I worked in what we have practice. I love this Class!!
+*/
     
- passKeys = {
+    
+ /**Decalared Variables
+ * I used the document method querySelector() and returned my diffrent imputs for the length,lowercaseletter,uppercaseletter and button.
+ * I also created a new passkey object to add keys to create the pattern to generate lowercase, uppercase, number and symbol based select option selected by user.
+ */
+    
+  var inputLength = document.querySelector('input[id="length"]');
+  var lowerCaseLetter = document.querySelector('input[id="lowercase"]');
+  var upperCaseLetter = document.querySelector('input[id="uppercase"]');
+  var number = document.querySelector('input[id="number"]');
+  var symbol = document.querySelector('input[id="symbol"]');
+  var generatePass = document.querySelector('.wrapper button');
+    
+  passKeys = {
   lowercase: 'abcdefghijklmnopqrstuvwxyz',
   uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
   number: '0123456789',
